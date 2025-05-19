5176c89a07e27538fc3374c70592139af9653721
pdobbins23 <146784496+pdobbins23@users.noreply.github.com>
2025-05-18 23:07:53 -0700

    Project setup

---
+++ src/Game.js
@@ -0,0 +1,17 @@
+class Game extends Phaser.Scene {
+  constructor() {
+    super("gameScene");
+  }
+
+  preload() {
+    
+  }
+
+  create() {
+    
+  }
+
+  update(time, deltaTime) {
+    
+  }
+}
